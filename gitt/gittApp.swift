c8fe5bb86c7ae2c8deb84579e9facc6137278247
AbrorNabiyev <abrornabiyev695@gmail.com>
2024-11-14 22:44:19 +0300

    Initial Commit

---
+++ gitt/gittApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittApp.swift
+//  gitt
+//
+//  Created by Abror on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct gittApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
